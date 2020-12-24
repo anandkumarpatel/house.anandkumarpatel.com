2962aa9c969b9f8e4456c821157f2141177be898
Anand Patel <anandkumarpatel@gmail.com>
2020-12-24 15:14:11 -0500

    add map and final copy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React, { useState, useCallback } from "react";
-import { render } from "react-dom";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
 import { photos } from "./photos.js"
@@ -47,6 +46,7 @@ function App() {
             ) : null}
           </ModalGateway>
         </div>
+        <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.4213364808193!2d-122.26454218429805!3d37.827020979750024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80857dfc53b0f777%3A0x1dbbe78ed014cfcc!2s446%2038th%20St%2C%20Oakland%2C%20CA%2094609!5e0!3m2!1sen!2sus!4v1608840011493!5m2!1sen!2sus" width="800" height="600" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         <div className="story">
           <h1>The story</h1>
           <p>The journey started with flight to San Francisco.
@@ -62,7 +62,10 @@ function App() {
           Just when we thought we had the house in the bag, drama hit.
           The listing agent was apparently rude to the appraiser and they refused to appraise the property.
           This put our loan at risk, we had a backup lender but it was a fully online lender so we are not sure how legit it is.
-          Will we get the house? Who knows.
+          Three days before closing we were finally approved! But we were not done yet, we had to sign the closing documents.
+          After we sent the documents it turns out some of the pages were blank. Someone had put X's on some of the places we needed to actually sign.
+          We rushed to our scanner & printer downstairs and sent the remaining documents.
+          On 12/11/2020 we finally closed! On to our adventures in Oakland!!!!
           </p>
         </div>
       </header>
